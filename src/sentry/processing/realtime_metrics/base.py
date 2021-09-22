ab5cb3857fb72bbc7b0726787bd81aedf5eb802b
Betty Da <bda@sentry.io>
2021-09-21 22:03:17 -0400

    fix types

--- src/sentry/processing/realtime_metrics/base.py
+++ src/sentry/processing/realtime_metrics/base.py
@@ -49,14 +49,13 @@ class RealtimeMetricsStore:
 
         Returns a list of project IDs.
         """
-        # TODO: there's got to be a better way to do this instead of invoking _to_int twice
-        return {
-            _to_int(project_id)
-            for project_id in self.inner.smembers(LPQ_MEMBERS_KEY)
-            if _to_int(project_id) is not None
-        }
+        return set(
+            filter(
+                None, {_to_int(project_id) for project_id in self.inner.smembers(LPQ_MEMBERS_KEY)}
+            )
+        )
 
-    def add_project_to_lpq(self, project_id: int):
+    def add_project_to_lpq(self, project_id: int) -> None:
         """
         Moves projects to the low priority queue.
 
@@ -103,7 +102,7 @@ class RealtimeMetricsStore:
         return was_removed
 
 
-def _to_int(value: str or int) -> Optional[int]:
+def _to_int(value: str) -> Optional[int]:
     try:
         return int(value) if value else None
     except ValueError:
